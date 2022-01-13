bb9fc765eda9177d88b761a093a1f9f8af52cc6b
Rukchad Wongprayoon <mooping3roblox@gmail.com>
2022-01-13 01:30:17 +0000

    p

--- BotJam.py
+++ BotJam.py
@@ -4,8 +4,6 @@ from utility import slash_util as slash
 
 bot = slash.Bot("j!",intents=discord.Intents().all()) 
 
-# do you have vs code?
-# i mean we can do a live share
-# and work on vs code is easier
-# and I have ai coding there too
-# no sorry :p
+@bot.event
+async def on_ready():
+    print("Bot started")
