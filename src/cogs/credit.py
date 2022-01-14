673cd1fed6558bece58b6de03a6606c658f1ab16
Rukchad Wongprayoon <mooping3roblox@gmail.com>
2022-01-14 10:21:47 +0000

    a

--- src/cogs/credit.py
+++ src/cogs/credit.py
@@ -9,7 +9,8 @@ class Credit(commands.Cog):
     async def credit(self, ctx):
         embed=nextcord.Embed(
             title="Information about Jampy",
-            description="This bot is a black-smithing bot made for TasosIsDev's 4th BotJam\n\n**IMportant notice: This bot is not a moderation-based bot, as it was made for thr bot Jam with the team: Blacksmith**",
+            description="This bot is a black-smithing bot made for TasosIsDev's 4th BotJam\n\n**Important notice: This bot is not a moderation-based bot, as it was made for thr bot Jam with the team: Blacksmith**",
+			color=0x
         )
         embed.add_field(
             name="test",
@@ -17,11 +18,11 @@ class Credit(commands.Cog):
         )
         embed.add_field(
             name="**test**",
-            value=""
+            value="a"
         )
         embed.add_field(
             name="&test*",
-            value=""
+            value="a"
         )
         await ctx.send(embed=embed)
 
