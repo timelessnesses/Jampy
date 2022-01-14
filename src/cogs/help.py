b4646813bc8f77b4fb2c03bffa64127eb698e55b
timelessnesses <mooping3roblox@gmail.com>
2022-01-14 17:26:41 +0700

    i forgor the send 💀

--- src/cogs/help.py
+++ src/cogs/help.py
@@ -9,15 +9,27 @@ class Help(commands.Cog):
     @commands.command(name='help')
     async def help(self, ctx, *, command: str = None):
         """Shows help about a command or the bot"""
-        embed = nextcord.Embed(title='Help', description='', color=nextcord.Color.blue())
-        for command in self.bot.commands:
-            if command.hidden:
-                continue
-            if command.aliases:
-                aliases = ' | '.join(command.aliases)
-                embed.add_field(name=f'{command.name} | {aliases}', value=command.help, inline=True)
-            else:
-                embed.add_field(name=command.name, value=command.help, inline=True)
+        if command is None:
+            embed = nextcord.Embed(title='Help', description='', color=nextcord.Color.blue())
+            for command in self.bot.commands:
+                if command.hidden:
+                    continue
+                if command.aliases:
+                    aliases = ' | '.join(command.aliases)
+                    embed.add_field(name=f'{command.name} | {aliases}', value=command.help, inline=True)
+                else:
+                    embed.add_field(name=command.name, value=command.help, inline=True)
+            await ctx.send(embed=embed)
+
+        else:
+            command = self.bot.get_command(command)
+            if command is None:
+                await ctx.send('That command does not exist.')
+                return
+            embed = nextcord.Embed(title=f'Help: {command.name}', description=command.help, color=nextcord.Color.blue())
+            embed.add_field(name='Usage', value=command.usage)
+            embed.add_field(name='Aliases', value=', '.join(command.aliases) if command.aliases else 'None')
+            await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Help(bot))
