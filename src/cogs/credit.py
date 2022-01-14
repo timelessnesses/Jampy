36996f550291a7869f3209a53ceda6ffb46e9e92
Rukchad Wongprayoon <mooping3roblox@gmail.com>
2022-01-14 11:14:39 +0000

    fard 🗿

--- src/cogs/credit.py
+++ src/cogs/credit.py
@@ -8,24 +8,27 @@ class Credit(commands.Cog):
     @commands.command(name='credit')
     async def credit(self, ctx):
         """
-        Shows the bot's credit.
+        Credits to everyone who contributed to this bot
         """
         embed=nextcord.Embed(
             title="Information about Jampy",
-            description="This bot is a black-smithing bot made for TasosIsDev's 4th BotJam\n\n**Important notice: This bot is not a moderation-based bot, as it was made for thr bot Jam with the team: Blacksmith**",
+            description="This bot is a Blacksmithing bot made for TasosIsDev's 4th BotJam\n\n**Important notice: This bot is not a moderation-based bot, as it was made for the bot Jam with the theme: Blacksmith**",
 			color=0xffaa00
         )
         embed.add_field(
-            name="test",
-            value="test"
+            name="Credits\n\nWho was the bot made by?",
+            value="Jampy was made by <@737103938192408637>,(737103938192408637) and <@890913140278181909>, (890913140278181909).",
+			inline=False
         )
         embed.add_field(
-            name="**test**",
-            value="a"
+            name="Icon and PFP",
+            value="The PFP orange backround represents heat while the font saying ''JAMPY'' represents IRON Being burnt. the idea was thought by Mooping and was made by Snaky.",
+			inline=False
         )
         embed.add_field(
-            name="&test*",
-            value="a"
+            name="Name",
+            value="The bot was going to be named ''Jam.py'' but then Snaky had a better idea to put all letters together.",
+			inline=False
         )
         await ctx.send(embed=embed)
 
