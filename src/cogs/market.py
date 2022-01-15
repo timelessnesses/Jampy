0d89833ca7630ee3fdbd0ae2eaa5948a41f26936
timelessnesses <mooping3roblox@gmail.com>
2022-01-15 13:35:57 +0700

    sell your fucking fruit leather or die

---
+++ src/cogs/market.py
@@ -0,0 +1,26 @@
+import nextcord
+from nextcord.ext import commands
+
+class Selling(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    async def initialize(self):
+        with open("src/cogs/db/db.json") as f:
+            db = json.load(f)
+        return db
+    
+    async def finalize(self, db):
+        with open("src/cogs/db/db.json", "w") as f:
+            json.dump(db, f, indent=4)
+    
+    @commands.command()
+    async def sell(self, ctx, item, amount):
+        db = await self.initialize()
+        if db[str(ctx.author.id)]["material"][item.lower()] >= amount:
+            db[str(ctx.author.id)]["material"][item.lower()] -= amount
+            db[str(ctx.author.id)]["money"] += amount
+            await ctx.send(embed=discord.Embed(title="Successfully sold {} {}(s)".format(amount, item), color=0xFFAA00))
+        else:
+            await ctx.send(embed=discord.Embed(title="You don't have enough {}(s)".format(item), color=0xFF0000))
+        await self.finalize(db)
