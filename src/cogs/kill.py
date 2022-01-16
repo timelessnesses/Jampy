20d1c37fb263c8b755744669b843f287410276aa
Code beautifier and Code linter <timelessnesses@users.noreply.github.com>
2022-01-16 03:47:17 +0000

    beautified and linted code you stupid nerd

--- src/cogs/kill.py
+++ src/cogs/kill.py
@@ -1,27 +1,30 @@
+import asyncio
+import json
+
 import nextcord
 from nextcord.ext import commands
-import json
-import asyncio
+
+
 class Kill(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
     async def initialize(self):
         await asyncio.wait(1)
         with open("src/cogs/db/db.json") as fp:
             db = json.load(fp)
         return db
-    
+
     async def initialize_sword(self):
         await asyncio.wait(1)
         with open("src/cogs/db/blacksmith.json") as fp:
             db = json.load(fp)
         return db
-    
+
     async def finalize(self, db):
         with open("src/cogs/db/db.json", "w") as fp:
             json.dump(db, fp, indent=4)
-    
+
     async def finalize_sword(self, db):
         with open("src/cogs/db/blacksmith.json", "w") as fp:
             json.dump(db, fp, indent=4)
@@ -40,8 +43,12 @@ class Kill(commands.Cog):
                         await ctx.send("Your sword is broken.")
                         return
                     sword["health"] -= db[str(ctx.author.id)]["damage"]
-                    await ctx.send(f"You killed {user.mention} with your {sword['name']}.")
+                    await ctx.send(
+                        f"You killed {user.mention} with your {sword['name']}."
+                    )
                     return
 
         else:
-            await ctx.send(f"What the actual hell? {user.name} is just a new guy! Leave him alone!")
+            await ctx.send(
+                f"What the actual hell? {user.name} is just a new guy! Leave him alone!"
+            )
